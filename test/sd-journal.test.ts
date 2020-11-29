13f312fe98af91495c8a03b35f4ada57504c7698
Sargun Dhillon <sargun@sargun.me>
2020-11-28 16:00:46 -0800

    Try to fix test skipping

--- test/sd-journal.test.ts
+++ test/sd-journal.test.ts
@@ -3,30 +3,32 @@ import { Map as ImmutableMap } from 'immutable'
 import child_process from 'child_process'
 import util from 'util'
 import fs from 'fs'
-let t = test;
-
-if (!fs.existsSync("/run/systemd/journal/socket") || journald.socket === null) {
-    console.log('Journald not enabled, skipping tests')
-    t = test.skip
-}
-
 const exec = util.promisify(child_process.exec)
 
-t('Basic log entry', async () => {
-    await journald.send(SyslogPrority.INFO, "testmessage", ImmutableMap({ "foo": "bar" }))
-    const { stdout, stderr } = await exec(`journalctl -o json MESSAGE=testmessage _PID=${process.pid}`)
-    const obj = JSON.parse(stdout)
-    expect(obj).toBeTruthy()
-    expect(stderr).toBeFalsy()
-})
+describe('Journald End-to-End Suite', () => {
+    let t = test
+    if (!fs.existsSync("/run/systemd/journal/socket") || journald.socket === null) {
+        console.log('Journald not enabled, skipping tests')
+        t = test.skip
+    }
 
 
-t('Newline log entry', async () => {
-    const LONG_VALUE = "test\nvalue"
-    await journald.send(SyslogPrority.INFO, "newlinemessage", ImmutableMap({ "LONG_VALUE": LONG_VALUE }))
-    const { stdout, stderr } = await exec(`journalctl -o json MESSAGE=newlinemessage _PID=${process.pid}`)
-    const obj = JSON.parse(stdout)
-    expect(obj).toBeTruthy()
-    expect(obj).toMatchObject({ 'LONG_VALUE': LONG_VALUE })
-    expect(stderr).toBeFalsy()
+    t('Basic log entry', async () => {
+        await journald.send(SyslogPrority.INFO, "testmessage", ImmutableMap({ "foo": "bar" }))
+        const { stdout, stderr } = await exec(`journalctl -o json MESSAGE=testmessage _PID=${process.pid}`)
+        const obj = JSON.parse(stdout)
+        expect(obj).toBeTruthy()
+        expect(stderr).toBeFalsy()
+    })
+
+
+    t('Newline log entry', async () => {
+        const LONG_VALUE = "test\nvalue"
+        await journald.send(SyslogPrority.INFO, "newlinemessage", ImmutableMap({ "LONG_VALUE": LONG_VALUE }))
+        const { stdout, stderr } = await exec(`journalctl -o json MESSAGE=newlinemessage _PID=${process.pid}`)
+        const obj = JSON.parse(stdout)
+        expect(obj).toBeTruthy()
+        expect(obj).toMatchObject({ 'LONG_VALUE': LONG_VALUE })
+        expect(stderr).toBeFalsy()
+    })
 })
